ec4ed11e6177dde8fd0007cdf4ae6ac6fc708e22
Jonathan <j.olaleye.o@gmail.com>
2018-06-27 13:59:27 -0500

    Reimplement sprites with Texture Packer

--- src/AssetManager.js
+++ src/AssetManager.js
@@ -1,21 +1,15 @@
-import { loader } from 'pixi.js';
+import { loader, Spritesheet } from 'pixi.js';
 
-import activePlayer from './assets/active-player.png';
-import activeGhost from './assets/active-ghost.png';
-import otherPlayer from './assets/other-player.png';
-import otherGhost from './assets/other-ghost.png';
+import atlas from './assets/atlas.json';
+import spritesheet from './assets/spritesheet.png';
 
 class AssetManager {
-  load = () => (
-    new Promise(resolve => {
-      loader
-        .add('activePlayer', activePlayer)
-        .add('activeGhost', activeGhost)
-        .add('otherPlayer', otherPlayer)
-        .add('otherGhost', otherGhost)
-        .load(() => resolve(loader.resources));
-    })
-  )
+  load = () => new Promise(resolve => {
+    loader.add(spritesheet).load(() => {
+      const sprites = new Spritesheet(loader.resources[spritesheet].texture.baseTexture, atlas);
+      sprites.parse(textures => resolve(textures));
+    });
+  });
 }
 
 export default new AssetManager();
