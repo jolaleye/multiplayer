730aaf1ff13099f5555f5382b90d74a9e5783519
Jonathan <j.olaleye.o@gmail.com>
2018-06-27 14:43:42 -0500

    Merge Game with GameContainer
    The appearance and logic of the Game component (a canvas)
    are tightly bound so keeping a separate
    presentational and container component is illogical.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,23 +1,27 @@
 import React, { Component, createRef } from 'react';
+import { Application } from 'pixi.js';
 
 import './Game.css';
 
 class Game extends Component {
   view = createRef();
+  app = null;
 
   componentDidMount() {
-    const { view, renderer } = this.props.app;
+    this.app = new Application({ antialias: true });
+
+    const { renderer, view } = this.app;
     this.view.current.appendChild(view);
 
     renderer.autoResize = true;
+    renderer.backgroundColor = 0x0a0a0a;
+
     window.addEventListener('resize', this.resize);
     this.resize();
-
-    renderer.backgroundColor = 0x0a0a0a;
   }
 
   resize = () => {
-    const { view, renderer } = this.props.app;
+    const { renderer, view } = this.app;
     const parent = view.parentNode;
     renderer.resize(parent.clientWidth, parent.clientHeight);
   }
