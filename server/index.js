c5f26b7a6fea825f7915be9f14d47243f83b4bbb
Jonathan <j.olaleye.o@gmail.com>
2018-06-27 21:19:03 -0500

    Add initial connection, simulation, and snapshot logic

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const express = require('express');
 const WebSocket = require('uws');
+const _ = require('lodash');
+
+const { ID } = require('./services/util');
+const Player = require('./Player');
 
 const app = express();
 const server = app.listen(3001);
@@ -10,3 +14,47 @@ app.use(express.static(path.join(__dirname, '../build')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
+
+let players = [];
+const commandQueue = [];
+
+ws.on('connection', client => {
+  // assign an ID
+  client.id = ID();
+  client.send(JSON.stringify({ _: 'clientID', id: client.id }));
+
+  // assign a player
+  client.player = new Player(client.id);
+  players.push(client.player);
+
+  client.on('message', packet => {
+    const data = JSON.parse(packet);
+    if (data._ === 'commands') {
+      data.targets.forEach(target => commandQueue.push({ id: client.id, target }));
+    }
+  });
+
+  // remove players when they disconnect
+  client.on('close', () => {
+    players = players.filter(({ id }) => id !== client.id);
+  });
+});
+
+// apply client requests
+const simulate = () => {
+  commandQueue.forEach(command => {
+    ws.clients.forEach(client => {
+      if (client.id === command.id) client.player.move(command.target);
+    });
+  });
+};
+
+// take a snapshot of the game state & send to clients
+const snapshot = () => {
+  ws.clients.forEach(client => {
+    client.send(JSON.stringify({ _: 'snapshot', timestamp: Date.now(), players }));
+  });
+};
+
+setInterval(simulate, 15);
+setInterval(snapshot, 50);
